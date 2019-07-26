ddd13f9e2dd33090f007a5ada1cd01b1d105c515
erika-vazquez <ito2016@protonmail.ch>
2019-07-26 13:05:06 -0500

    Part 1 complete

---
+++ src/components/EpisodeCard.js
@@ -0,0 +1,5 @@
+import React from 'react'
+
+export default function EpisodeCard ({ name, airDate, episode }) {
+  return (<span>todo: location</span>)
+  }
--- src/components/EpisodeList.js
+++ src/components/EpisodeList.js
@@ -1,29 +1,29 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios'
 
-export default function LocationsList() {
+export default function EpisodeList() {
   // TODO: Add useState to track data from useEffect
-const [locations, setLocations] = useState([]);
+const [episodes, setEpisodes] = useState([]);
   useEffect(() => {
     // TODO: Add AJAX/API Request here - must run in `useEffect`
     //  Important: verify the 2nd `useEffect` parameter: the dependancies array!
     axios
-    .get('https://rickandmortyapi.com/api/location/')
+    .get('https://rickandmortyapi.com/api/episode/')
     .then(response =>{
       console.log(response.data.results)
-      setLocations(response.data.results);
+      setEpisodes(response.data.results);
     })
   
   }, []);
 
   return( <section className='character-list grid-view'>
   {/*TODO: `array.map()` over your state here!{*/}
-  {locations.map((location, index)=>{
+  {episodes.map((episode, index)=>{
     return (
-      <div className="location-card">
-      <h2> {location.name}</h2>
-      <h3> {location.type}</h3>
-      <h3>Residents: {location.residents.length}</h3>
+      <div className="episode-card">
+      <h2> {episode.name}</h2>
+      <h3> {episode.air_date}</h3>
+      <h3>Episode: {episode.episode}</h3>
 
      </div>
     )})
