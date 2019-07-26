a3d21b821907e70103fc283b8859bc092e87bfd5
erika-vazquez <ito2016@protonmail.ch>
2019-07-26 12:39:11 -0500

    Added characters component to display info about every character

--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -1,16 +1,35 @@
 import React, { useEffect, useState } from 'react';
+import axios from 'axios'
 
 export default function CharacterList() {
   // TODO: Add useState to track data from useEffect
-
+const [chars, setChars] = useState([]);
   useEffect(() => {
     // TODO: Add AJAX/API Request here - must run in `useEffect`
     //  Important: verify the 2nd `useEffect` parameter: the dependancies array!
-  }, [])
+    console.log("hello")
+    axios
+    .get('https://rickandmortyapi.com/api/character/')
+    .then(response =>{
+      console.log(response.data.results)
+      setChars(response.data.results);
+    })
+  
+  }, []);
 
-  return <section className='character-list grid-view'>
-
-      <h2>TODO: `array.map()` over your state here!</h2>
+  return( <section className='character-list grid-view'>
+  {/*TODO: `array.map()` over your state here!{*/}
+  {chars.map((character, index)=>{
+    return (
+      <div className="char-card">
+      <img className="card-img" src={character.image}></img>
+      <h2>{character.name}</h2>
+      <p>{character.species}</p>
+      <h3>Location: {character.location.name}</h3>
+      <h3>Origin: {character.origin.name}</h3>
+     </div>
+    )})
+  }
     </section>
-
+  )
 }
--- src/components/LocationsList.js
+++ src/components/LocationsList.js
@@ -1,5 +1,31 @@
 import React, { useEffect, useState } from 'react';
+import axios from 'axios'
 
 export default function LocationsList() {
+  // TODO: Add useState to track data from useEffect
+const [locations, setLocations] = useState([]);
+  useEffect(() => {
+    // TODO: Add AJAX/API Request here - must run in `useEffect`
+    //  Important: verify the 2nd `useEffect` parameter: the dependancies array!
+    axios
+    .get('https://rickandmortyapi.com/api/location/')
+    .then(response =>{
+      console.log(response.data.results)
+      setLocations(response.data.results);
+    })
+  
+  }, []);
 
+  return( <section className='character-list grid-view'>
+  {/*TODO: `array.map()` over your state here!{*/}
+  {locations.map((location, index)=>{
+    return (
+      <div className="location-card">
+      <h3> {location.name}</h3>
+      <h3>Planet: {location.type}</h3>
+     </div>
+    )})
+  }
+    </section>
+  )
 }
